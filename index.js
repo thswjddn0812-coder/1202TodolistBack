bcca475f7ccc24f12089b88e2de9d2842c987397
Son <thswjddn0812@gmail.com>
2025-12-03 14:53:15 +0900

    cors 설정

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(cors({
+  origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/', routes);
