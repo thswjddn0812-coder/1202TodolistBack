0d7aeb54576f97862bc128b5891ddd6aa67eff63
Son <thswjddn0812@gmail.com>
2025-12-03 14:57:55 +0900

    localhost 수정

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
+  origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
   credentials: true
 }));
 app.use(express.json());
